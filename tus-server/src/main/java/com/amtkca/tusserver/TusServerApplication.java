b965652c1aae2e772fbcd3411fde267bdb538fb6
amtkxa <scarlet0211@outlook.jp>
2019-04-20 01:05:55 +0900

    feat: (back-end) create project

---
+++ tus-server/src/main/java/com/amtkca/tusserver/TusServerApplication.java
@@ -0,0 +1,13 @@
+package com.amtkca.tusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusServerApplication.class, args);
+    }
+
+}
